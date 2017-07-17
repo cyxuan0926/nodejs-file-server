50d3ec867e35028799ad17b3229616a6d9551b14
David Liu <liurui81@gmail.com>
2017-07-17 09:33:02 +0800

    init image-server

---
+++ app.js
@@ -0,0 +1,53 @@
+const logger = require('log4js').getLogger('app'),
+    express = require('express'),
+    multer = require('multer'),
+    path = require('path'),
+    app = express();
+
+const storage = multer.diskStorage({
+        destination: (req, file, cb) => {
+            switch (req.path) {
+                case '/profile':
+                    cb(null, 'public/avatars');
+                    break;
+                case '/topics':
+                    cb(null, 'public/topics');
+                    break;
+                default:
+                    logger.debug('this path has no operation');
+            }
+        },
+        filename: (req, file, cb) => {
+            cb(null, `${file.originalname}`);
+        }
+    }),
+    fileFilter = (req, file, cb) => {
+        if (file.mimetype == 'image/jpeg') cb(null, true);
+        else cb(new Error('unallowed file type'), false);
+    },
+    limits = { fileSize: 1024 * 1000, parts: 9 },
+
+    upload = multer({
+        storage: storage,
+        fileFilter: fileFilter,
+        limits: limits
+    });
+
+app.use(express.static(__dirname + '/public'));
+
+const uploadAvatar = upload.single('avatar');
+app.post('/profile', function(req, res) {
+    uploadAvatar(req, res, (err) => {
+        if (err) {
+            logger.error(err.message);
+            res.status(500).send({ code: 500, status: 'ERROR', message: err.message });
+            return;
+        }
+
+        res.send({ code: 200, status: 'SUCCESS' });
+    });
+});
+
+app.listen(1339, () => {
+    logger.info('app server listening on port 1339');
+});
